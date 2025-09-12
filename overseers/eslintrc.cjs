d1ee564a3705b290b0a55112f845e986c5b0f3a1
tagueteamtrucking-afk <tagueteamtrucking@gmail.com>
2025-09-11 17:41:03 -0700

    Delete overseers directory

--- overseers/eslintrc.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  root: true,
-  env: { node: true, es2022: true },
-  extends: ['eslint:recommended', 'prettier'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  rules: {}
-};
