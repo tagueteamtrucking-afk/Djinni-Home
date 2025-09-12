2c280d3001daa2b81e123f9b14f8837a59c29332
tagueteamtrucking-afk <tagueteamtrucking@gmail.com>
2025-09-11 17:15:01 -0700

    Create eslintrc.cjs

---
+++ overseers/eslintrc.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  root: true,
+  env: { node: true, es2022: true },
+  extends: ['eslint:recommended', 'prettier'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  rules: {}
+};
